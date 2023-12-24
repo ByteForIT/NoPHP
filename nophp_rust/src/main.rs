afb9fd5856ef55c878481e1369cc98ad079d19fa
Artur Z <doslunar8@gmail.com>
2023-12-24 23:49:32 +0100

    Borrowcheckr

--- nophp_rust/src/main.rs
+++ nophp_rust/src/main.rs
@@ -1,17 +1,20 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyTuple};
+use std::env;
 
 fn main() -> PyResult<()> {
+    let args: Vec<String> = env::args().collect();
+
     let py_foo = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
         "/../lang/lexer.py"
     ));
     let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../lang/lexer.py"));
-    let from_python = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
+    let from_python = Python::with_gil(move |py| -> PyResult<Py<PyAny>> {
         PyModule::from_code(py, py_foo, "lexer", "lexer")?;
         let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
             .getattr("run")?
             .into();
-        app.call0(py)
+        app.call1(py, PyTuple::new(py, vec![args[0]]))
     });
 
     println!("py: {}", from_python?);
---
+++ nophp_rust/test.php
@@ -0,0 +1,147 @@
+<?php 
+
+namespace App;
+
+class Router {
+    public function __construct() {
+        echo "Im init!";
+        echo '<br>';
+    }
+    public function myFunction() : int {
+        echo "Hello from inside myFunction in class Router!";
+        echo '<br>';
+        $returns = 10; 
+        return $returns;
+    }
+}
+
+function RunTests($id) : void {
+    $hello = "Hello ";    
+
+    $example_html = '<h1>' . $hello . "World" . '</h1>';
+
+    echo $example_html;
+
+    echo '<code>';
+
+    function ExampleFunction() : int {
+        echo "I return: ";
+        return 1;
+    }
+
+    $hi = ExampleFunction();
+
+    echo $hi;
+
+    echo '<br>';
+
+    $router = new Router();
+    
+
+    $b = $router->myFunction();
+
+    // If
+    if ($b == 10) {
+        echo "If works";
+        echo '<br>';
+    } else {
+        echo "If failed";
+        // ?panic;
+    }
+
+    // While
+    while ($b != 10) {
+        echo $b;
+        $b = $b + 1;
+    }
+    
+    $array = ["Hello", "World", "three"];
+
+    // Foreach
+    foreach ($array as $value) {
+        echo $value;
+        echo '<br>';
+    }
+
+echo '</code>';
+}
+
+RunTests(1);
+
+class Robot
+{
+    public $greeting;
+    // $greeting = "a";
+
+    public function __construct()
+    {
+        $this->greeting = "Hello";
+    }
+
+	public function greet()
+	{
+		return $this->greeting;
+	}
+
+    public function bye() 
+    {
+        return 'Good bye!';
+    }
+    public function wrap()
+    {
+        echo "+++++++";
+    }
+}
+
+class Android extends Robot
+{
+    public function __construct()
+    {
+        // ? debug;
+        $this->greeting = "Hi!";
+    }
+
+}
+
+// Instead of this
+// debug_print_backtrace();
+// Use this
+// ?debug;
+
+$robot = new Robot();
+$a = $robot->greet(); // Hello
+echo "Robot: " . $a . " Expected: " . $robot->greeting;
+echo "</br>";
+
+
+$android = new Android();
+$b = $android->greet(); // Hi!
+echo "Android: " . $b . " Expected: " . $android->greeting;
+echo "</br>";
+$android->bye(); // Good bye!
+
+require_once("example.php");
+
+use Example;
+
+echo Example\greet();
+
+class YourApp {
+    public $greeting;
+    public function __construct() {
+        $this->greeting = "Welcome to NoPHP";
+    } 
+    public function welcome() {
+        aaa();
+        echo <h1> $this->greeting </h1>;
+    }
+}
+$app = new YourApp();
+$app->welcome();
+// ?debug;
+
+?>
+
+<p>
+Hello World!!
+</p>
