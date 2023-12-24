81be46e5e2f39e1f217df61bf3b6f98bb05175da
Artur Z <doslunar8@gmail.com>
2023-12-25 00:49:45 +0100

    Few fixes to get the basic version working

--- nophp_rust/src/main.rs
+++ nophp_rust/src/main.rs
@@ -6,17 +6,34 @@ fn main() -> PyResult<()> {
 
     let py_foo = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/../lang/lexer.py"
+        "/py/doodle.py"
     ));
-    let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/../lang/lexer.py"));
-    let from_python = Python::with_gil(move |py| -> PyResult<Py<PyAny>> {
-        PyModule::from_code(py, py_foo, "lexer", "lexer")?;
-        let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
-            .getattr("run")?
+    let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/py/doodle.py"));
+    let from_python = Python::with_gil(move |py| -> Py<PyAny> {
+        PyModule::from_code(py, py_foo, "parser", "parser").unwrap();
+        let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")
+            .unwrap()
+            .getattr("run")
+            .unwrap()
             .into();
-        app.call1(py, PyTuple::new(py, vec![args[1].clone()]))
+        let f1 = app.call1(py, PyTuple::new(py, vec![args[1].clone()]));
+
+        match &f1 {
+            Err(err) => {
+                    println!(
+                    "{}", err.traceback(py)
+                    .unwrap()
+                    .format()
+                    .unwrap()
+                );
+                panic!();
+            }
+            Ok(_) => {
+                return f1.unwrap();
+            }
+        };
     });
 
-    println!("py: {}", from_python?);
+    println!("py: {}", from_python);
     Ok(())
 }
